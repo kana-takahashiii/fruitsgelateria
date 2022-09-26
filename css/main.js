fc465b4e7b880a99dbe5ec1e10fe554fa202b81e
kana-takahashiii <kana92105@gmial.com>
2022-09-26 17:48:33 +0900

    js　spanコメントアウト削除

--- css/main.js
+++ css/main.js
@@ -66,29 +66,27 @@ gsap.from('.card-item',
     });
 
 
+// -------------
 // span化
+// -------------
 class SpanWrap {
     constructor(target) {
     
         this.target = this.convertElement(target);
         this.nodes = [...this.target.childNodes];
         this.convert();
-        // console.log("spanでた");
     }
-    
     convert() {
     
         let spanWrapText = ""
     
         this.nodes.forEach((node) => {
-        if (node.nodeType == 3) {//テキストの場合
-            const text = node.textContent.replace(/\r?\n/g, '');//テキストから改行コード削除
-            //spanで囲んで連結
+        if (node.nodeType == 3) {
+            const text = node.textContent.replace(/\r?\n/g, '');
             spanWrapText = spanWrapText + text.split('').reduce((acc, v) => {
             return acc + `<span>${v}</span>`
             }, "");
-        } else {//テキスト以外
-            //<br>などテキスト以外の要素をそのまま連結
+        } else {
             spanWrapText = spanWrapText + node.outerHTML
         }
         })
@@ -96,14 +94,10 @@ class SpanWrap {
         this.target.innerHTML = spanWrapText
     
     }
-    //jQueryオブジェクトや文字列セレクターを変換
     convertElement(element) {
         if (element instanceof HTMLElement) {
             return element
         }
-        // if (element instanceof jQuery) {
-        //   return element[0]
-        // }
         return document.querySelector(element);
         }
 }
