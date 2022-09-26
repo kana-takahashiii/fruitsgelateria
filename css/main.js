498a30f6316c2ef1c72fe61dde2265b93dfa0210
kana-takahashiii <kana92105@gmial.com>
2022-09-26 17:05:24 +0900

    p 　span化

--- css/main.js
+++ css/main.js
@@ -64,3 +64,66 @@ gsap.from('.card-img-wrapper img',
             // markers: true
         }
     });
+
+
+// span化
+class SpanWrap {
+    constructor(target) {
+    
+        this.target = this.convertElement(target);
+        this.nodes = [...this.target.childNodes];
+        this.convert();
+        // console.log("spanでた");
+    }
+    
+    convert() {
+    
+        let spanWrapText = ""
+    
+        this.nodes.forEach((node) => {
+        if (node.nodeType == 3) {//テキストの場合
+            const text = node.textContent.replace(/\r?\n/g, '');//テキストから改行コード削除
+            //spanで囲んで連結
+            spanWrapText = spanWrapText + text.split('').reduce((acc, v) => {
+            return acc + `<span>${v}</span>`
+            }, "");
+        } else {//テキスト以外
+            //<br>などテキスト以外の要素をそのまま連結
+            spanWrapText = spanWrapText + node.outerHTML
+        }
+        })
+    
+        this.target.innerHTML = spanWrapText
+    
+    }
+    //jQueryオブジェクトや文字列セレクターを変換
+    convertElement(element) {
+        if (element instanceof HTMLElement) {
+            return element
+        }
+        // if (element instanceof jQuery) {
+        //   return element[0]
+        // }
+        return document.querySelector(element);
+        }
+}
+//実行
+const targets = [...document.querySelectorAll(".main-copy")]
+targets.forEach( (target) => {
+    new SpanWrap(target);
+})
+
+
+
+// // main-copy
+// gsap.set(".main-copy", {
+//     opacity: 0,
+// }
+// );
+// var El = gsap.timeline();
+//     El.to(".main-copy span",{
+//         opacity: 1,
+//         duration: 
+//         stagger: 0.07,
+//         // ease: "Expo.easeOut"
+//     })
